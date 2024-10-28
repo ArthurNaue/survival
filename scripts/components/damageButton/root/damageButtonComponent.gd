777356f51a2479b15449b820e19c1c37976bc386
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-10-28 12:00:11 -0300

    ConstructionManager Progress

--- scripts/components/damageButton/root/damageButtonComponent.gd
+++ scripts/components/damageButton/root/damageButtonComponent.gd
@@ -4,5 +4,5 @@ class_name damageButtonComponent
 @export var healthComponent: HealthComponent
 
 func _on_pressed() -> void:
-	if healthComponent:
+	if healthComponent and ConstructionManager.buildMode == false:
 		healthComponent.damage(1)
--- scripts/managers/constructionManager/root/constructionManager.gd
+++ scripts/managers/constructionManager/root/constructionManager.gd
@@ -10,41 +10,59 @@ var constructions := {
 var buildMode := false
 var constructionSprite: Sprite2D
 var constructionObject: ConstructionObjectsStats
+var tutorialText: RichTextLabel
 
 func _process(_delta: float) -> void:
 	if ConstructionManager.buildMode == true:
+		tutorialText.global_position = Vector2(get_global_mouse_position().x - 40, get_global_mouse_position().y)
 		constructionSprite.global_position = get_global_mouse_position()
-		if get_player().gui.visible == false:
+		if get_player().gui.visible == false and check_requirements():
 			constructionSprite.modulate = Color.GREEN
 		else:
 			constructionSprite.modulate = Color.RED
 
 func spawn_construction_object(object: ConstructionObjectsStats, location: Vector2) -> void:
-	var canBuild := false
-	if PlayerManager.check_resources(object.firstRequirement) and  PlayerManager.check_resources(object.secondRequirement):
+	if check_requirements():
 		PlayerManager.update_resources(object.firstRequirement, "-")
 		PlayerManager.update_resources(object.secondRequirement, "-")
 		var construction = constructionObjectScene.instantiate() as ConstructionObject
 		construction.stats = object
 		construction.global_position = location
+		construction.sprite.rotation_degrees = constructionSprite.rotation_degrees
 		WorldManager.get_world().add_child(construction)
 		turn_build_mode_off()
 
 func turn_build_mode_on(object: ConstructionObjectsStats) -> void:
 	buildMode = true
-	constructionObject = object
+	tutorialText = RichTextLabel.new()
+	tutorialText.text = "Q / CANCEL
+R / ROTATE"
+	tutorialText.size = Vector2(150, 50)
+	tutorialText.scale = Vector2(0.25, 0.25)
 	constructionSprite = Sprite2D.new()
-	constructionSprite.global_position = get_global_mouse_position()
 	constructionSprite.texture = object.sprite
+	constructionObject = object
 	add_child(constructionSprite)
+	add_child(tutorialText)
 
 func turn_build_mode_off() -> void:
 	buildMode = false
 	constructionSprite.queue_free()
+	tutorialText.queue_free()
+
+func check_requirements() -> bool:
+	var requirementsMet := false
+	if PlayerManager.check_resources(constructionObject.firstRequirement) and PlayerManager.check_resources(constructionObject.secondRequirement):
+		requirementsMet = true
+	return requirementsMet
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("M1") and ConstructionManager.buildMode == true and get_player().gui.visible == false:
+	if Input.is_action_just_pressed("M1") and buildMode == true and get_player().gui.visible == false:
 		spawn_construction_object(constructionObject, get_global_mouse_position())
+	if Input.is_action_just_pressed("Q") and buildMode == true:
+		turn_build_mode_off()
+	if Input.is_action_just_pressed("R") and buildMode == true:
+		constructionSprite.rotation_degrees += 90
 
 func get_player() -> CharacterBody2D:
 	return get_tree().get_first_node_in_group("player")
