c9feb3e0f8a8d4dcd0989d6fa3f67af95b27a647
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2024-11-30 13:16:45 -0300

    ajustes na flecha que aponta pra campfire

--- scripts/arrow/root/arrow.gd
+++ scripts/arrow/root/arrow.gd
@@ -3,9 +3,8 @@ extends Node2D
 @onready var campfire := WorldManager.get_world().get_node("campfire")
 
 func _process(_delta: float) -> void:
-	var distance = global_position.distance_to(campfire.global_position)
-	if distance <= 150:
-		visible = false
-	else:
+	if PlayerManager.get_player().get_node("gui").visible:
 		visible = true
+	else:
+		visible = false
 	look_at(campfire.global_position)
